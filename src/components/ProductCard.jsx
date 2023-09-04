4dcb21ba217bec71a669d37b564be02b94411abb
AndyKallian <andersonkallian@yahoo.com.br>
2023-09-04 19:28:18 -0300

    responsive width

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ const buttonClass = 'absolute bottom-0 left-1/2 transform -translate-x-1/2 trans
 
 const ProductCard = ({image, product, description}) => {
   return (
-    <div className='sm:w-11/12 w-[360px] h-[484px] rounded-2xl shadow-custom flex flex-col items-center justify-center relative'>
+    <div className='sm:w-11/12 md:w-[360px] h-[484px] rounded-2xl shadow-custom flex flex-col items-center justify-center relative'>
       <div>
         <img src={image} alt="" className=' h-[290px]' />
         <h2 className='text-center'>{product}</h2>
--- src/components/TryOnComponent.jsx
+++ src/components/TryOnComponent.jsx
@@ -5,22 +5,22 @@ import Button from './Button'
 
 const TryOnComponent = () => {
   return (
-    <div className='bg-green-500 flex items-center justify-evenly sm:flex-col-reverse md:flex-row sm:h-[374px] md:h-[290px] -z-10 relative'>
+    <div className='bg-green-500 flex items-center justify-evenly sm:flex-col-reverse md:flex-row sm:h-[374px] md:h-[290px] sm:-z-0 md:-z-10 relative'>
 
       <div className='flex gap-5'>
-        <img src={lataGuarana} alt="" className='sm:hidden h-[177px] rotate-45' />
-        <img src={lataGuarana} alt=""  className='sm:hidden h-[343px] w-[195px] mt-28 -rotate-12'/>
+        <img src={lataGuarana} alt="" className='sm:hidden md:block h-[177px] rotate-45' />
+        <img src={lataGuarana} alt=""  className='sm:hidden md:block h-[343px] w-[195px] mt-28 -rotate-12'/>
       </div>
       
-      <div className='flex flex-col gap-3 items-center'>
+      <div className='flex flex-col gap-3 sm:h-[145px] md:h-auto items-center'>
         <h2 className='font-vollkorn text-[24px] font-bold leading-[31px]'>Quer experimentar?</h2>
         <Button
           buttonClass={'bg-black text-white cursor-pointer'}
         />
       </div>
 
-      <div>
-        <img src={lataGuarana} alt=""  className='sm:h-[262px] sm:w-[149px] sm:rotate-0 h-[503px] w-[286px] bottom rotate-12'/>
+      <div className='sm:h-[262px] sm:w-[149px] sm:rotate-0 h-[503px] w-[286px] bottom rotate-12 top-0 sm:-translate-y-20 md:-translate-y-0'>
+        <img src={lataGuarana} alt=""/>
       </div>
 
     </div>
